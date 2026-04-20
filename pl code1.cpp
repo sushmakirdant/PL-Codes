47a9c305c29f261a8c4d8e653e9e3bf7d55d59d4
sushmakirdant <sushmakirdant70@gmail.com>
2026-04-20 17:18:40 +0530

    Add files via upload

---
+++ pl code1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, result;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    result = a - b;
+
+    cout << "Subtraction = " << result;
+
+    return 0;
+}
